6e2fd76129a6209df56bcddea55fded61c5873fd
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2022-03-20 16:02:49 +1100

    Adjust value

--- app/services/twitter/tweet.ts
+++ app/services/twitter/tweet.ts
@@ -23,7 +23,7 @@ export async function tweet(body: Cast, offset: number, random: number) {
 
     let shieldHealth = "✨ Shield Health: " + body.shield_health.toString()
 
-    let heading = text[offset].title + ": " + (body.token_amount * (body.token_boost / 1000 + 1))
+    let heading = text[offset].title + ": " + (body.token_amount * (body.token_boost / 100 + 1))
 
     let msg = description + "\n \n \n" + heading + "\n \n" + cityHealth + "\n \n" + shieldHealth
 
