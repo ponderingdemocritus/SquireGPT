b7385ba673c23dca682ad0c36d9295cdb8367d88
tenoxious <tenox.0274@gmail.com>
2022-08-19 09:12:24 -0700

    feat: raid public feed

--- app/config/index.ts
+++ app/config/index.ts
@@ -1,25 +1,31 @@
-require('dotenv').config();
+require("dotenv").config();
 
 export const twitterConfig = {
-    consumer_key: process.env.CONSUMER_KEY || "",
-    consumer_secret: process.env.CONSUMER_SECRET || "",
-    access_token: process.env.ACCESS_TOKEN_KEY || "",
-    access_token_secret: process.env.ACCESS_TOKEN_SECRET || "",
+  consumer_key: process.env.CONSUMER_KEY || "",
+  consumer_secret: process.env.CONSUMER_SECRET || "",
+  access_token: process.env.ACCESS_TOKEN_KEY || "",
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET || ""
 };
 
 export const discordConfig = {
-    prefix: "!",
-    client_id: process.env.DISCORD_CLIENT_ID || "",
-    guild_id: process.env.DISCORD_GUILD_ID || "",
-    token: process.env.DISCORD_TOKEN || "",
-    salesChannel: process.env.DISCORD_SALES_CHANNEL,
-    listingsChannel: process.env.DISCORD_LISTINGS_CHANNEL,
-}
+  prefix: "!",
+  client_id: process.env.DISCORD_CLIENT_ID || "",
+  guild_id: process.env.DISCORD_GUILD_ID || "",
+  token: process.env.DISCORD_TOKEN || "",
+  salesChannel: process.env.DISCORD_SALES_CHANNEL,
+  listingsChannel: process.env.DISCORD_LISTINGS_CHANNEL,
+  raidsChannel: process.env.DISCORD_RAIDS_CHANNEL
+};
 
 export const openSeaConfig = {
-    openseaAssetUrl: "https://api.opensea.io/api/v1/asset",
-    openseaEventsUrl: "https://api.opensea.io/api/v1/events",
-    openseaApiKey: process.env.OPEN_SEA_API_KEY || "",
-    collectionName: process.env.OPEN_SEA_COLLECTION_NAME,
-    contractAddress: process.env.REALMS_CONTRACT_ADDRESS
-}
+  openseaAssetUrl: "https://api.opensea.io/api/v1/asset",
+  openseaEventsUrl: "https://api.opensea.io/api/v1/events",
+  openseaApiKey: process.env.OPEN_SEA_API_KEY || "",
+  collectionName: process.env.OPEN_SEA_COLLECTION_NAME,
+  contractAddress: process.env.REALMS_CONTRACT_ADDRESS
+};
+
+export const biblioConfig = {
+  indexerUrl: "https://dev-indexer-gu226.ondigitalocean.app/graphql",
+  atlasBaseUrl: "https://settling.bibliothecadao.xyz"
+};
